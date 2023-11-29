9c4dda0b04300ada14cf0a16951df61470ef0e60
meAltf <alataf.dce.cs1@gmail.com>
2023-11-29 21:47:21 +0530

    Implemented User Input/Output problem

--- 1-Learn the Basics/1.1- Things to Know in Java or any other languge/User InputOutput.java
+++ 1-Learn the Basics/1.1- Things to Know in Java or any other languge/User InputOutput.java
@@ -1,14 +1,18 @@
 import java.util.*;
+
 public class Solution {
-    
+
     public static void main(String[] args) {
         // Write your code here
-        
+
         Scanner sc = new Scanner(System.in);
         char input = sc.next().charAt(0);
 
-        if(input >= 'A' && input <= 'Z') System.out.println(1);
-        else if(input >= 'a' && input <= 'z') System.out.println(0);
-        else System.out.println(-1);
+        if (input >= 'A' && input <= 'Z')
+            System.out.println(1);
+        else if (input >= 'a' && input <= 'z')
+            System.out.println(0);
+        else
+            System.out.println(-1);
     }
 }
