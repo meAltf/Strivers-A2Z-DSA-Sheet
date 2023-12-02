d17d21dd66f7e99d2d988bac38ba3595819b5774
meAltf <alataf.dce.cs1@gmail.com>
2023-12-03 00:08:56 +0530

    Update in this code

--- 1-Learn the Basics/1.1- Things to Know in Java or any other languge/switchCaseStatement.java
+++ 1-Learn the Basics/1.1- Things to Know in Java or any other languge/switchCaseStatement.java
@@ -1,13 +1,13 @@
 public class Solution {
     public static double areaSwitchCase(int ch, double[] a) {
         // Write your code here
-        double result = 0;
+        double result = 0.0;
         switch (ch) {
             case 1:
-                result = 3.14 * a[0] * a[0];
+                result = Math.PI * a[0] * a[0];
+                break;
             case 2:
-                if (a.length >= 2)
-                    result = a[0] * a[1];
+                result = a[0] * a[1];
         }
         return result;
     }
