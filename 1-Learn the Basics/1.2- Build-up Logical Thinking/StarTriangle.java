fe463e49b899f83f8f1c085190a606ec5ba55089
meAltf <alataf.dce.cs1@gmail.com>
2024-02-15 02:11:47 +0530

    pattern-7 Updated

--- 1-Learn the Basics/1.2- Build-up Logical Thinking/StarTriangle.java
+++ 1-Learn the Basics/1.2- Build-up Logical Thinking/StarTriangle.java
@@ -1,3 +1,6 @@
+/**
+ * Using 4 loops, printing * seperatly part wise
+ */
 public class Solution {
     public static void nStarTriangle(int n) {
         // Write your code here
@@ -19,6 +22,35 @@ public class Solution {
                 System.out.print(" ");
             }
             System.out.println();
+        }
+    }
+}
+
+/**
+ * using 3 loops only, printing * together row wise
+ */
+public class Solution {
+    public static void nStarTriangle(int n) {
+        // Write your code here
+        // using 3 loop only, printing (*) row wise at once.
+        for (int i = 0; i < n; i++) {
+            // For printing the spaces before stars in each row
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            // For printing the stars in each row
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print("*");
+            }
+            // For printing the spaces after the stars in each row
+            for (int x = 0; x < n - i - 1; x++) {
+                System.out.print(" ");
+            }
+            // As soon as the stars for each iteration are printed, we move to the
+            // next row and give a line break otherwise all stars
+            // would get printed in 1 line.
+            System.out.println();
+
         }
     }
 }
