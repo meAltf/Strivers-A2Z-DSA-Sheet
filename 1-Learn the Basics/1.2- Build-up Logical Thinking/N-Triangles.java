414f883f901a838bfa3ee3718110fb1fd4e546a2
meAltf <alataf.dce.cs1@gmail.com>
2024-02-14 23:18:45 +0530

    Pattern-3

---
+++ 1-Learn the Basics/1.2- Build-up Logical Thinking/N-Triangles.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public static void nTriangle(int n) {
+        // Write your code here
+
+        for (int i = 0; i < n; i++) {
+            int num = 1;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(num + " ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+}
