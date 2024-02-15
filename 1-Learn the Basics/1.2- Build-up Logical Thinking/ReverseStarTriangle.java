7f44a069085ca7d5245345223684c159470dec15
meAltf <alataf.dce.cs1@gmail.com>
2024-02-15 22:00:10 +0530

    pattern-8 Updated

---
+++ 1-Learn the Basics/1.2- Build-up Logical Thinking/ReverseStarTriangle.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public static void nStarTriangle(int n) {
+        // Write your code here
+
+        for (int i = 0; i < n; i++) {
+            // for starting of the space
+            for (int k = 0; k < i; k++) {
+                System.out.print(" ");
+            }
+            // for star printing
+            for (int j = 0; j < 2 * n - (2 * i + 1); j++) {
+                System.out.print("*");
+            }
+            // for ending of the space
+            for (int x = 0; x < i; x++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
