52469c8a50243c286ff375ceeaf4441d0bd92d68
meAltf <alataf.dce.cs1@gmail.com>
2024-02-17 13:28:13 +0530

    Updated Pattern-14 with char loop

--- 1-Learn the Basics/1.2- Build-up Logical Thinking/IncreasingLetterTriangle.java
+++ 1-Learn the Basics/1.2- Build-up Logical Thinking/IncreasingLetterTriangle.java
@@ -3,12 +3,20 @@ public class Solution {
         // Write your code here
         for (int i = 0; i < n; i++) {
             char printChar = 'A';
-            for (int j = 0; j <= i; j++) {
-                System.out.print(printChar + " ");
-                printChar++;
+            // updating Loop with character
+            for (char ch = 'A'; ch <= 'A' + i; i++) {
+                System.out.println(ch);
             }
+            /**
+             * 
+             * for (int j = 0; j <= i; j++) {
+             * System.out.print(printChar + " ");
+             * printChar++;
+             * }
+             * 
+             **/
             System.out.println();
         }
     }
 }
-// updated with character..
+// updated with character....
