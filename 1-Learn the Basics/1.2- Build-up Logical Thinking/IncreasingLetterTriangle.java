abb8beee63215a45ebe786650172dd1a23a49188
meAltf <alataf.dce.cs1@gmail.com>
2024-02-17 13:12:24 +0530

    pattern-13

---
+++ 1-Learn the Basics/1.2- Build-up Logical Thinking/IncreasingLetterTriangle.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public static void nLetterTriangle(int n) {
+        // Write your code here
+        for (int i = 0; i < n; i++) {
+            char printChar = 'A';
+            for (int j = 0; j <= i; j++) {
+                System.out.print(printChar + " ");
+                printChar++;
+            }
+            System.out.println();
+        }
+    }
+}
