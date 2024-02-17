1503e029fda9876f1118c6a176f2a497d5b87623
meAltf <alataf.dce.cs1@gmail.com>
2024-02-17 13:32:09 +0530

    Pattern-15 | Updated | with char for loop

--- 1-Learn the Basics/1.2- Build-up Logical Thinking/ReverseLetterTriangle.java
+++ 1-Learn the Basics/1.2- Build-up Logical Thinking/ReverseLetterTriangle.java
@@ -4,10 +4,17 @@ public class Solution {
         for (int i = 0; i < n; i++) {
             char printChar = 'A';
             // for character printing
-            for (int j = 0; j < n - i; j++) {
-                System.out.print(printChar + " ");
-                printChar++;
+            for (char ch = 'A'; ch < 'A' + (n - i - 1); ch++) {
+                System.out.print(ch + " ");
             }
+            /*
+             * 
+             * for (int j = 0; j < n - i; j++) {
+             * System.out.print(printChar + " ");
+             * printChar++;
+             * }
+             * 
+             */
             // for spaces
             for (int k = 0; k < i; k++) {
                 System.out.print(" ");
