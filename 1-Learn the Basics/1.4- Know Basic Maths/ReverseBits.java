db270d3d52221fe21d07915a9d6da30d537013d7
meAltf <alataf.dce.cs1@gmail.com>
2024-02-25 14:54:05 +0530

    Revrse Bits || CN || Added Approach

--- 1-Learn the Basics/1.4- Know Basic Maths/ReverseBits.java
+++ 1-Learn the Basics/1.4- Know Basic Maths/ReverseBits.java
@@ -20,3 +20,19 @@ public class Solution {
         return ans;
     }
 }
+
+// Approach-
+/*
+ * 1- initializes an array arr of size 32 to store the individual bits of the
+ * number in reverse order.
+ * 2- Iterates over the number n, taking its remainder when divided by 2 (n % 2)
+ * and storing it in the array arr.
+ * 3- It then divides n by 2 to shift its bits to the right (n / 2) and
+ * increments the index i.
+ * 4- Once all bits of n are extracted and stored in arr, it pads the rest of
+ * the array with zeros if necessary to make it exactly 32 bits long.
+ * 5- then constructs the reversed number by iterating over the array arr in
+ * reverse order.
+ * 6- Starting from the most significant bit (arr[31]), it multiplies each bit
+ * by increasing powers of 2 and accumulates the result in the variable ans.
+ */
